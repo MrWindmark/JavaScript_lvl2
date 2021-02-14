ba3ddb5f1e0df2d6c64f5d54637eaf0468d29a7f
mrwindmark <agentstorm@ya.ru>
2021-02-14 23:50:51 +0300

    некоторые подвижки в работе с корзиной и её элементами

--- main.js
+++ main.js
@@ -11,13 +11,51 @@ class ShopItem {
     }
 }
 
-class GoodsBasket{
+class ShopList {
     constructor () {
-        this.gBasket = [];
+        this.shopList = [];
     }
 
-    addItem(item){
-        this.gBasket.push(item);
+    addItem(limit) {
+        for (let i = 0; i < limit; i++) {
+            let tmp = new ShopItem(`Item_${1}`, 10*(i+1), `url_${i}`, 10000+i*10, 2);
+            console.log(tmp);
+            this.shopList.push(tmp);
+        }
+    }
+
+    pickItem(id) {
+        return this.shopList[id];
+    }
+    checkLength () {
+        return this.shopList.length;
+    }
+}
+
+class NewBasketItem {
+    /**
+     * 
+     * @param {ShopItem} item: ShopItem
+     * @param {Number} quantity: Number
+     */
+    constructor (item, quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+}
+
+class GoodsBasket{
+    constructor () {
+        this.gBasket = {};
+    }
+
+    addGood(item){
+        let tmpItem = new NewBasketItem(item, 1);
+        if (!this.gBasket[item.id]){
+            this.gBasket[item.id] = tmpItem;
+        } else {
+            this.gBasket[item.id].quantity += 1;
+        }
     }
 
     dropItem(item) {
@@ -28,22 +66,26 @@ class GoodsBasket{
         let tPrice = 0;
         console.log(this.gBasket.length);
         for(let i = 0; i < this.gBasket.length; i++){
-            tPrice += this.gBasket[i].price;
+            tPrice += this.gBasket[i].price * this.gBasket[i].quantity;
         }
         return tPrice;
     }
-
-    createBasket (limit) {
-        for (let i = 0; i < limit; i++) {
-            let tmp = new ShopItem(`Item_${1}`, 10*i, `url_${i}`, 10000+i*10);
-            console.log(tmp);
-            basket.addItem(tmp);
-        }
-    }
 }
 
+let list1 = new ShopList;
+list1.addItem(100);
+
+console.log('list: ', list1.checkLength());
+
 let basket = new GoodsBasket;
-basket.createBasket(100);
+basket.addGood(list1.pickItem(0));
+console.log('el1: ', list1.pickItem(0));
+basket.addGood(list1.pickItem(1));
+console.log('el2: ', list1.pickItem(1));
+basket.addGood(list1.pickItem(2));
+console.log('el3: ', list1.pickItem(2));
+
+console.log('basket:', basket);
 let total = basket.totalPrice();
 console.log('total: ', total);
  
