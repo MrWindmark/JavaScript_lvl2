58668e921cf6151030885232f0452759d7deac27
mrwindmark <agentstorm@ya.ru>
2021-02-28 21:50:06 +0200

    Созданы компоненты для: корзины, поиска (нет перерисовки, есть вывод в консоль), вывода списка товаров и вывода одного товара

--- vue-main.js
+++ vue-main.js
@@ -1,113 +1,18 @@
-const goodsItem = [
-    {
-        id: '1',
-        price: '10',
-        name: 'Ban',
-        img: '#1'
-    },
-    {
-        id: '2',
-        price: '20',
-        name: 'Kaba',
-        img: '#2'
-    },
-    {
-        id: '3',
-        price: '30',
-        name: 'Chooh',
-        img: '#3'
-    },
-    {
-        id: '4',
-        price: '45',
-        name: 'Loom',
-        img: '#4'
-    },
-    {
-        id: '5',
-        price: '53',
-        name: 'Bam',
-        img: '#5'
-    },
-    {
-        id: '6',
-        price: '62',
-        name: 'Mendel',
-        img: '#6'
-    },
-    {
-        id: '7',
-        price: '70',
-        name: 'PanBan',
-        img: '#7'
-    },
-    {
-        id: '8',
-        price: '80',
-        name: 'Kaa',
-        img: '#8'
-    },
-    {
-        id: '9',
-        price: '90',
-        name: 'ChihPih',
-        img: '#9'
-    },
-    {
-        id: '10',
-        price: '95',
-        name: 'Boom',
-        img: '#10'
-    },
-    {
-        id: '11',
-        price: '124',
-        name: 'Bambook',
-        img: '#11'
-    },
-    {
-        id: '12',
-        price: '162',
-        name: 'End',
-        img: '#12'
-    },
-]
-
 const BASE_URL = 'https://mock-api-builder.vercel.app/api/schema/get';
 
-const app = new Vue({
-    el: '#app',
-    data: {
-        goodsList: [],
-        filteredGoods: [],
-        searchKey: '',
-        isVisibleCart: false,
-        cartName: 'Корзина',
+Vue.component('basket-app', {
+    template: `<div class="basket">
+                    <button @click='switchState()'>{{cartName}}</button>
+                    <div class="cart-block" v-if="isVisibleCart">Hello</div>
+                </div>`,
+    props: ['filteredGoods'],
+    data() {
+        return {
+            isVisibleCart: false,
+            cartName: 'Корзина',
+        }
     },
     methods: {
-        getItems() {
-            return fetch(`${BASE_URL}/602c166a89c4a60009ef7046`)
-            .then(result => result.json())
-            .then(result =>{
-                this.goodsList = result;
-                this.filteredGoods = this.goodsList;
-                console.log(result);
-            })
-            .catch((e) => {
-                console.log('Error', e)
-            });
-        },
-        serachClick() {
-            let result = this.filteredGoods.filter(i => i.productName.toLowerCase().includes(this.searchKey.toLowerCase()));
-            console.log(result);
-            return result;
-        },
-        clickLMB(id) {
-            console.log("We're on", id);
-        },
-        FilterGoods(value) {
-            // pass
-        },
         switchState() {
             this.isVisibleCart = !this.isVisibleCart;
             console.log('Now', this.isVisibleCart);
@@ -117,7 +22,70 @@ const app = new Vue({
             if(this.isVisibleCart) {
                 this.cartName = 'Свернуть';
             }
+        },
+    }
+});
+
+Vue.component('search-app', {
+    template: ` <div class="search-app">
+                    <input type="text" v-model="searchKey" placeholder="search">
+                    <button @click="filterGoods()">Go Search!</button>
+                </div>`,
+    props: { goods: Array },
+    data() {
+        return {
+            searchKey: '',
         }
+    },
+    methods: {
+        filterGoods() {
+            console.log(this.goods.filter(i => i.productName.toLowerCase().includes(this.searchKey.toLowerCase())));
+            return this.goods.filter(i => i.productName.toLowerCase().includes(this.searchKey.toLowerCase()));
+        },
+    }
+});
+
+Vue.component('list-app', {
+    props: ['goods'],
+    template: ` <div class="shop-item">
+                    <item-draw-app v-for="item in goods" :item="item" :key="item.id"></item-draw-app>
+                </div>`,
+});
+
+Vue.component('item-draw-app', {
+    props: ['item'],
+    template: ` <div class="item-element">
+                    <!-- <img :src="item.img" alt=""> -->
+                    <h2>{{item.productName}}</h2>
+                    <span>{{item.price}}</span>
+                    <button @click='clickLMB(item.id)'>Buy</button>
+                    <hr/>
+                </div>`,
+    methods: {
+        clickLMB(id) {
+            console.log("We're on", id);
+        },
+    },
+});
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        goodsList: [],
+        filteredGoods: [],
+    },
+    methods: {
+        async getItems() {
+            try {
+                const result = await fetch(`${BASE_URL}/602c166a89c4a60009ef7046`);
+                const result_1 = await result.json();
+                this.goodsList = result_1;
+                this.filteredGoods = this.goodsList;
+                console.log(result_1);
+            } catch (e) {
+                console.log('Error', e);
+            }
+        },
     },
     mounted(){
         this.getItems();
