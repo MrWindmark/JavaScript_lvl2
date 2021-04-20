f8abc39ab2354878670152cefa3ee49ee7f7252f
mrwindmark <agentstorm@ya.ru>
2021-04-20 13:27:43 +0300

    makeGETRequest done

--- async.js
+++ async.js
@@ -17,20 +17,20 @@
 //     xhr.send();
 //   }
 // ********************************************************
-// а вот это уже я пытался разобраться, оно работает... странно
-// const makeGETRequest = (myUrl) => {
-//     fetch(myUrl)
-//         .then(response => {
-//             if (response.ok) {
-//                 return response.json();
-//             }
-//         })
-//         .catch (err => {console.log(err)})
-//         .then(result => {console.log(result)})
-// }
+// а вот это уже я пытался разобраться, и оно работает
+const makeGETRequest = (myUrl) => {
+    fetch(myUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .catch (err => {console.info('Erron:', err, 'URL:', myUrl)})
+        .then(result => {console.info('Result value:', result, 'URL:', myUrl)})
+}
 
-// const urlAPI = 'https://jsonplaceholder.typicode.com/todos/';
-// const urlAPI2 = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json';
+const urlAPI = 'https://jsonplaceholder.typicode.com/todos/';
+const urlAPI2 = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json';
 
-// makeGETRequest(urlAPI);
-// makeGETRequest(urlAPI2);
+makeGETRequest(urlAPI);
+makeGETRequest(urlAPI2);
